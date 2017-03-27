eee7431af49eff472805908d79e2b3f68c572616
lhy <lhy@lhydeMacBook-Pro.local>
2017-03-27 14:07:13 +0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by lhy on 2017/3/27.
+//  Copyright © 2017年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by lhy on 2017/3/27.
+//  Copyright © 2017年 lhy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
